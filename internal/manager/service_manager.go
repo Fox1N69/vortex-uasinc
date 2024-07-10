71b7348bc94c9a5da2681ea9000fce12abffb062
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-07-10 18:11:47 +0500

    Implement client service to the service manager

--- internal/manager/service_manager.go
+++ internal/manager/service_manager.go
@@ -1,10 +1,13 @@
 package manager
 
 import (
+	"sync"
 	"test-task/infra"
+	service "test-task/internal/services"
 )
 
 type ServiceManager interface {
+	ClientService() service.ClientService
 }
 
 type serviceManager struct {
@@ -18,4 +21,18 @@ func NewServiceManager(infra infra.Infra) ServiceManager {
 		infra: infra,
 		repo:  NewRepoManager(infra),
 	}
+}
+
+var (
+	clientServiceOnce sync.Once
+	clientService     service.ClientService
+)
+
+func (sm *serviceManager) ClientService() service.ClientService {
+	clientServiceOnce.Do(func() {
+		clientRepo := sm.repo.ClientRepository()
+		clientService = service.NewClientService(clientRepo)
+	})
+
+	return clientService
 }
