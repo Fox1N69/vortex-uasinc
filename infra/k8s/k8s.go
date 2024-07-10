3ddb1820d72fbebc64ae9d4a2fafdcaecebc9045
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-07-10 17:14:37 +0500

    Implemented methods in the k8s.go

--- infra/k8s/k8s.go
+++ infra/k8s/k8s.go
@@ -1,2 +1,33 @@
 package k8s
 
+import (
+	"fmt"
+	"os/exec"
+)
+
+type KubernetesDeployer struct{}
+
+func NewKubernetesDeployer() *KubernetesDeployer {
+	return &KubernetesDeployer{}
+}
+
+func (k *KubernetesDeployer) CreatePod(name string) error {
+	cmd := exec.Command("kubectl", "run", name, "--image=nginx")
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to create pod: %w", err)
+	}
+	return nil
+}
+
+func (k *KubernetesDeployer) DeletePod(name string) error {
+	cmd := exec.Command("kubectl", "delete", "pod", name)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to delete pod: %w", err)
+	}
+
+	return nil
+}
+
+func (k *KubernetesDeployer) GetAllPodList() ([]string, error) {
+	return []string{"pod1", "pod2"}, nil
+}
